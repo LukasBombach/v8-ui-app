320a81575567cf7014451c1e0d9c30fde2caf0f5
Lukas Bombach <lukas.bombach@googlemail.com>
2021-08-04 23:00:30 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cell::{RefCell, RefMut};
 use std::collections::HashMap;
 use std::rc::Rc;
 
@@ -12,63 +13,43 @@ use winit::{
     window::{Window, WindowId},
 };
 
-/* struct WindowManager {
-    event_loop: EventLoop<()>,
-    windows: HashMap<WindowId, Window>,
-}
-
-impl WindowManager {
-    pub fn new() -> WindowManager {
-        WindowManager {
-            event_loop: EventLoop::new(),
-            windows: HashMap::new(),
-        }
-    }
-
-    pub fn add(&mut self) {
-        let window = Window::new(&self.event_loop).unwrap();
-        self.windows.insert(window.id(), window);
-    }
-
-    pub fn remove(&mut self, id: WindowId) {
-        self.windows.remove(&id);
-    }
-
-    pub fn run_loop<F>(&mut self, event_handler: F) -> !
-    where
-        F: 'static + FnMut(Event<'_, ()>, &mut ControlFlow, &mut WindowManager),
-    {
-        self.event_loop
-            .run(|event, event_loop, control_flow| event_handler(event, control_flow, self))
-    }
-} */
-
 fn main() {
     SimpleLogger::new().init().unwrap();
 
-    // let mut window_manager = WindowManager::new();
     let mut runtime = JsRuntime::new(Default::default());
-    let event_loop = EventLoop::new();
-    let mut windows: HashMap<WindowId, Window> = HashMap::new();
 
-    /* runtime.register_op(
+    // let event_loop = EventLoop::new();
+    let event_loop = Rc::new(EventLoop::new());
+
+    // let mut windows: HashMap<WindowId, Window> = HashMap::new();
+    // let windows_rc = Rc::new(RefCell::new(windows));
+
+    let windows: Rc<RefCell<HashMap<WindowId, Window>>> = Rc::new(RefCell::new(HashMap::new()));
+
+    runtime.register_op(
         "openWindow",
         op_sync(|_state, msg: Option<String>, _: ()| {
-            window_manager.add();
+            let event_loop_c = Rc::clone(&event_loop);
+            let mut windows_c: RefMut<_> = windows.borrow_mut();
+            let window = Window::new(&event_loop_c).unwrap();
+            windows_c.insert(window.id(), window);
             Ok(())
         }),
-    ); */
+    );
 
     runtime.sync_ops_cache();
 
-    event_loop.run(move |event, _event_loop, control_flow| {
+    let event_loop_c = Rc::clone(&event_loop);
+
+    event_loop_c.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
             Event::WindowEvent { event, window_id } => match event {
                 WindowEvent::CloseRequested => {
                     println!("Window {:?} has received the signal to close", window_id);
-                    windows.remove(&window_id);
+                    // let mut windows: RefMut<_> = windows.borrow_mut();
+                    // windows.remove(&window_id);
                 }
                 _ => (),
             },
