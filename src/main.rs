7e1062d7b94f86f15e0869bc93ee89c25810c57b
Lukas Bombach <lukas.bombach@googlemail.com>
2021-08-05 18:16:56 +0200

    tabularasa and threaded js runtime

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,65 @@
-mod window_manager;
+use std::{collections::HashMap, thread, time::Duration};
 
-use deno_core::op_sync;
-use deno_core::JsRuntime;
-use simple_logger::SimpleLogger;
-use window_manager::{Event, WindowEvent, WindowManager};
+use winit::{
+  event::{Event, WindowEvent},
+  event_loop::{ControlFlow, EventLoop},
+  window::Window,
+};
+
+use deno_core::{op_sync, JsRuntime};
+
+// use simple_logger::SimpleLogger;
+
+#[derive(Debug, Clone, Copy)]
+enum CustomEvent {
+  CreateWindow,
+}
 
 fn main() {
-    SimpleLogger::new().init().unwrap();
+  // SimpleLogger::new().init().unwrap();
 
+  let event_loop = EventLoop::<CustomEvent>::with_user_event();
+  let event_loop_proxy = event_loop.create_proxy();
+
+  let mut windows = HashMap::new();
+  let window = Window::new(&event_loop).unwrap();
+  windows.insert(window.id(), window);
+
+  thread::spawn(move || {
+    thread::sleep(Duration::from_secs(3));
     let mut runtime = JsRuntime::new(Default::default());
-    let mut window_manager = WindowManager::new();
-
-    // runtime.register_op(
-    //     "openWindow",
-    //     op_sync(|_state, msg: Option<String>, _: ()| {
-    //         window_manager.add();
-    //         Ok(())
-    //     }),
-    // );
-
+    runtime.register_op(
+      "openWindows",
+      op_sync(move |_state, num_windows: i32, _: ()| {
+        let mut n = 1;
+        while n < num_windows {
+          event_loop_proxy.send_event(CustomEvent::CreateWindow).ok();
+          n += 1;
+        }
+        Ok(())
+      }),
+    );
     runtime.sync_ops_cache();
 
-    window_manager.run_loop(move |event, window_manager| {
-        match event {
-            Event::WindowEvent { event, window_id } => match event {
-                WindowEvent::CloseRequested => {
-                    println!("Window {:?} has received the signal to close", window_id);
-                    // let mut windows: RefMut<_> = windows.borrow_mut();
-                    window_manager.remove(window_id);
-                }
-                _ => (),
-            },
-            _ => (),
-        }
-    })
+    runtime
+      .execute_script("test", "Deno.core.opSync('openWindows', 4);")
+      .unwrap();
+  });
+
+  event_loop.run(move |event, event_loop, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::UserEvent(event) => {
+        println!("user event: {:?}", event);
+        let window = Window::new(&event_loop).unwrap();
+        windows.insert(window.id(), window);
+      }
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => (),
+    }
+  });
 }
