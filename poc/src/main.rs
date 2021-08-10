93a37a7673e576ba2fcf6e711f12759313ae49ee
Lukas Bombach <lukas.bombach@googlemail.com>
2021-08-10 18:36:24 +0200

    added multiwindow

--- poc/src/main.rs
+++ poc/src/main.rs
@@ -1,6 +1,8 @@
-use js_runtime;
+// use js_runtime;
+use gui_runtime;
 
 fn main() {
-    let js_code = include_str!("test.js");
-    js_runtime::run(js_code);
+    // let js_code = include_str!("test.js");
+    // js_runtime::run(js_code);
+    gui_runtime::run();
 }
