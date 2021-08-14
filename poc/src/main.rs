1aaa656a076895f9d6ab0678cd3349949cf4df2e
Lukas Bombach <lukas.bombach@googlemail.com>
2021-08-14 14:13:36 +0200

    implemented a window mananger

--- poc/src/main.rs
+++ poc/src/main.rs
@@ -1,55 +1,53 @@
-use js_runtime_deno;
+// use js_runtime_deno;
 
-use std::path::Path;
-use std::thread;
+// use std::path::Path;
+// use std::thread;
 
 use std::collections::HashMap;
 
 use winit::event::Event;
-use winit::event::StartCause;
+// use winit::event::StartCause;
 use winit::event::WindowEvent;
 use winit::event_loop::ControlFlow;
 use winit::event_loop::EventLoop;
+use winit::event_loop::EventLoopWindowTarget;
 use winit::window::Window;
+// use winit::window::WindowBuilder;
 use winit::window::WindowId;
 
-#[derive(Debug, Clone, Copy)]
-enum CustomEvent {
-    CreateWindow,
+struct WindowManager<'a, T: 'static> {
+    event_loop: &'a EventLoopWindowTarget<T>,
+    windows: HashMap<WindowId, Window>,
+}
+
+impl<T> WindowManager<'_, T> {
+    fn new(event_loop: &EventLoopWindowTarget<T>) -> WindowManager<'_, T> {
+        WindowManager {
+            event_loop,
+            windows: HashMap::new(),
+        }
+    }
+
+    fn add(&mut self) {
+        let window = Window::new(self.event_loop).unwrap();
+        self.windows.insert(window.id(), window);
+    }
 }
 
 fn main() {
-    let event_loop = EventLoop::<CustomEvent>::with_user_event();
-    let mut windows: HashMap<WindowId, Window> = HashMap::new();
+    let el = EventLoop::new();
+    let mut wm = WindowManager::new(&el);
 
-    let event_loop_proxy = event_loop.create_proxy();
+    wm.add();
 
-    let js_thread = thread::spawn(move || {
-        thread::park();
-
-        js_runtime_deno::run("src/test.js");
-
-        // js_runtime::run(js_code, move |event| match event {
-        //     js_runtime::Event::CreateWindow => {
-        //         event_loop_proxy.send_event(CustomEvent::CreateWindow).ok();
-        //     }
-        // });
-    });
-
-    event_loop.run(move |event, event_loop, control_flow| {
+    el.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
+        println!("{:?}", event);
 
         match event {
-            Event::NewEvents(StartCause::Init) => {
-                js_thread.thread().unpark();
-            }
-            Event::UserEvent(CustomEvent::CreateWindow) => {
-                let window = Window::new(&event_loop).unwrap();
-                windows.insert(window.id(), window);
-            }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
-                ..
+                window_id: _,
             } => *control_flow = ControlFlow::Exit,
             _ => (),
         }
